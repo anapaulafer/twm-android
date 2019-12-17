b2c439cf60ff1c6306587fe93592a24e36e656e2
Ana Paula <anapaulaferreira_95@hotmail.com>
2019-12-17 12:23:34 -0300

    first commit'

---
+++ app/src/main/java/com/example/shoponline/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.shoponline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imagem1= (ImageView)findViewById(R.id.imageView4);
+
+    }
+}
